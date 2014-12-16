f2467da246e7736dfe72118b0938839eda5764c4
Moldován Eduárd <edimoldovan@gmail.com>
2014-12-16 23:41:56 +0100

    Basic things
    - first layout
    - node app with livereload and scss compile
    - based on skeleton css

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-one-pager
-=========
+nappi
+=====
 
-Small utility for creating responsive one pegers
+This is very simple node.js application, which currently provides a REST API basically out of the box.
+
+Here's a todo list, that I would like to implement soon:
+- scalable authentication
+- mysql support (currently only mongo available)
---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express"),
+  app = express(),
+  sassMiddleware = require("node-sass-middleware"),
+  livereload = require("express-livereload");
+
+livereload(app, config={});
+
+app.use(sassMiddleware({
+    src: __dirname + "/public",
+    dest: __dirname + "/public",
+    debug: true,
+    outputStyle: "compressed"
+  })
+);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", function(req, res) {
+  "use strict";
+  res.sendFile(__dirname + "/public/html/index.html");
+});
+
+app.listen(3000);
